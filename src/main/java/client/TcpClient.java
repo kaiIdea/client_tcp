efd28d350be0ab2bff2523a2c7d74e58d414a900
GEEX1428 <yangkaikai@geexfinance.com>
2021-11-08 18:35:08 +0800

    Initialized client and server

---
+++ src/main/java/client/TcpClient.java
@@ -0,0 +1,9 @@
+package main.java.client;
+
+/**
+ * @Auther: GEEX1428
+ * @Date: 2021/11/8 18:33
+ * @Description:
+ */
+public class TcpClient {
+}
---
+++ src/main/java/server/TcpServer.java
@@ -0,0 +1,9 @@
+package main.java.server;
+
+/**
+ * @Auther: GEEX1428
+ * @Date: 2021/11/8 18:34
+ * @Description:
+ */
+public class TcpServer {
+}
