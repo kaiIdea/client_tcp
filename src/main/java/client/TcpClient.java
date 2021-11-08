1c59c57bf5601fae7c17027db7a3d1d737138089
GEEX1428 <yangkaikai@geexfinance.com>
2021-11-08 19:15:50 +0800

    example TCP Request commit...

--- src/main/java/client/TcpClient.java
+++ src/main/java/client/TcpClient.java
@@ -1,9 +1,41 @@
 package main.java.client;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 /**
  * @Auther: GEEX1428
  * @Date: 2021/11/8 18:33
- * @Description:
+ * @Description: TCP客户端
  */
 public class TcpClient {
+
+    public static void main(String[] args) throws IOException {
+        //设置host
+        String host = "127.0.0.1";
+        //设置端口
+        int port = 8888;
+        //获取控制台输入数据流
+        BufferedReader readerUser = new BufferedReader(new InputStreamReader(System.in));
+        //根据host,port创建连接
+        Socket socket = new Socket(host,port);
+        String text = readerUser.readLine();
+
+        //获取socket内部持有输出流
+        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        //以字节格式，输出数据到server
+        dataOutputStream.writeBytes(text+'\n');
+
+
+        //读取server处理后的数据
+        BufferedReader readerServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String text1 = readerServer.readLine();
+        //打印
+        System.out.println("from server:"+text1);
+        //关闭连接
+        socket.close();
+    }
 }
--- src/main/java/server/TcpServer.java
+++ src/main/java/server/TcpServer.java
@@ -1,9 +1,32 @@
 package main.java.server;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @Auther: GEEX1428
  * @Date: 2021/11/8 18:34
- * @Description:
+ * @Description: TCP服务端
  */
 public class TcpServer {
+
+    public static void main(String[] args) throws IOException {
+        //设置监听端口
+        int port = 8888;
+        //创建server服务,内部方法做bind，端口绑定，监听
+        ServerSocket serverSocket = new ServerSocket(port);
+        //获取socket连接
+        Socket socket = serverSocket.accept();
+        //获取socket输入流
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String text =reader.readLine();
+        System.out.println(text);
+        //获取socket 内部持有的输出流
+        DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+        //对客户端传送过来的数据做处理
+        String text1= text.toUpperCase()+'\n';
+        //讲处理过后的数据，以字节格式写回流中，传回客户端
+        outputStream.writeBytes(text1);
+    }
 }
